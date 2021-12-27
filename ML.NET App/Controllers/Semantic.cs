7e48b8364b9f48bd75062da581d04c9462000ddf
Sterben-Universe <med.boulaich@hotmail.com>
2021-12-27 01:21:07 +0100

    initial commit

---
+++ ML.NET App/Controllers/Semantic.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using Microsoft.ML;
+using static ML_NET_App.SentimentAnalysis;
+
+namespace ML.NET_App.Controllers
+{
+    public class Semantic : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Analysis()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Analysis(ModelInput input)
+        {
+            MLContext mlContext = new MLContext();
+
+            ITransformer mlModel = mlContext.Model.Load(@"..\SentimentAnalysis_WebApp\SentimentAnalysis.zip", out var modelInputSchema);
+
+            var predEngine = mlContext.Model.CreatePredictionEngine<ModelInput, ModelOutput>(mlModel);
+
+            input.Year = DateTime.Now.Year;
+
+            ModelOutput result = predEngine.Predict(input);
+
+            ViewBag.Result = result;
+
+            return View();
+        }
+    }
+}
